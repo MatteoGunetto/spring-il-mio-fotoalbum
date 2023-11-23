d8eeab3bc7a60afcd32254a193b53df0bcdf98ec
MatteoGunetto <matteogunetto@tiscali.it>
2023-11-23 09:47:13 +0100

    first commit

---
+++ src/test/java/org/project/java/fotoAlbum/SpringIlMioFotoalbumApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.java.fotoAlbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIlMioFotoalbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
